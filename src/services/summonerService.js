411ff6d1f0a0e3b652feabdd658e72c6afb36c5e
ematthiasen <89933203+ematthiasen@users.noreply.github.com>
2022-07-25 20:06:24 +0200

    Add basic functionality
    -Still no permanent RIOT api key
    -add championService
    -add summonerService

---
+++ src/services/summonerService.js
@@ -0,0 +1,33 @@
+import axios from 'axios'
+import env from 'react-dotenv'
+
+//const baseUrl = 'https://developer.riotgames.com/'
+const baseUrl = 'http://localhost:8080/https://euw1.api.riotgames.com'
+
+const getSummoner = async (summonerName) => {
+  const headerConfig = {
+    headers: {
+      'X-Riot-Token': env.RIOT_API_KEY
+   }
+  }
+  const result = await axios.get(`${baseUrl}/lol/summoner/v4/summoners/by-name/${summonerName}`, headerConfig)
+  console.log(result.data)
+  return(result.data)
+}
+
+const getSummonerMasteries = async (encryptedSummonerId) => {
+  const headerConfig = {
+    headers: {
+      'X-Riot-Token': env.RIOT_API_KEY
+   }
+  }
+
+  const result = await axios.get(`${baseUrl}/lol/champion-mastery/v4/champion-masteries/by-summoner/${encryptedSummonerId}`, headerConfig)
+  console.log(result.data)
+  return(result.data)
+
+}
+
+const exports = { getSummoner, getSummonerMasteries }
+
+export default exports
