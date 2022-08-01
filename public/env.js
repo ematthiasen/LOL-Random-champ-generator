1500054f93260ed14ff4684acff2a4a36e56ef7f
ematthiasen <89933203+ematthiasen@users.noreply.github.com>
2022-08-01 22:13:11 +0200

    Use grid and lists to display information

--- public/env.js
+++ public/env.js
@@ -1,4 +1,4 @@
 window.env = {
-  "RIOT_API_KEY": "RGAPI-1423f322-a966-475f-977b-b4a5cb20a033",
+  "RIOT_API_KEY": "RGAPI-483db7c6-98a3-4e8d-8e5c-157aed029cbc",
   "LOCAL_CORS": "http://127.0.0.1:8080"
 };
---
+++ src/components/BenchList.js
@@ -0,0 +1,46 @@
+import { Box, List, ListItem, ListSubheader } from '@mui/material'
+import SummonerSmall from './SummonerSmall'
+import { Droppable } from 'react-beautiful-dnd'
+
+const BenchList = ({teamList, summoners, getChampData, drawerWidth}) => {
+
+  return (
+
+      <Droppable droppableId={teamList.id} direction='vertical'>
+        
+        {(provided) => (
+          <Box sx={{ width: 1, height: 1 }}>
+            <List
+              ref={provided.innerRef}
+              {...provided.droppableProps}
+              sx={{ height: 1}}
+            >
+              <ListSubheader>
+                Bench
+              </ListSubheader>
+              {teamList.summoners.map((summonerId, index) => {
+                const summoner = summoners[summonerId]
+                return (
+                  <SummonerSmall
+                    key={summoner.id}
+                    summoner={summoner}
+                    getChampData={getChampData}
+                    index={index}
+                  >
+        
+                  </SummonerSmall>
+                )
+              })}
+              {provided.placeholder}
+              <ListItem>
+                Bottom of the list
+              </ListItem>
+            </List>
+          </Box>
+        )}
+      </Droppable>
+
+  )
+}
+
+export default BenchList
