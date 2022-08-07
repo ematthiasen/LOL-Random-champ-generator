cc0222919293a2ebb607488b53a0f8c27a7a2b0d
ematthiasen <89933203+ematthiasen@users.noreply.github.com>
2022-08-07 19:47:33 +0200

    hardcode url for render.com

--- frontend/src/services/socketService.js
+++ frontend/src/services/socketService.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client'
 
-const socketBaseUrl = '/'
+const socketBaseUrl = 'http://aramrandom.onrender.com:3003/'
 const socket = io(socketBaseUrl)
 
 const socketConnect = (setData, setSummonerLoading, displaySnackbarMessage, setMinMasteryCutoff, setMaxMasteryCutoff) => {
